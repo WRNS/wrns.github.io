9a326e222ee39c0d07bec035061b292a2b473bc2
Ralph Bean <rbean@redhat.com>
2016-10-30 16:17:48 -0400

    Nicer announcements.

--- js/announcements.js
+++ js/announcements.js
@@ -2,22 +2,35 @@ var announcements = [
     {
       idx: 'open-enrollment-2016-2017',
       message: 'Enrollment is open for the 2016-2017 school year.  Apply today!',
-      url: 'forms/wrns-application.pdf',
+      url: '#enroll',
+      icon: 'paperclip',
     },
     {
       idx: 'under-construction',
       message: "This site is under construction.  Email me at webmaster@wintonroad.org with suggestions, please!",
       url: 'mailto:webmaster@wintonroad.org',
+      icon: 'code-fork',
     },
 ];
 $.each(announcements, function(i, a) {
   if (Cookies.get(a.idx) != undefined) return;
   var blurb = '<div class="announcement alert alert-dismissible alert-danger">' +
-      '<button type="button" class="close" data-dismiss="alert" ' +
+      '<button type="button" class="close btn btn-sm mybutton_standard" data-dismiss="alert" ' +
         'onclick="javascript:Cookies.set(\'' + a.idx + '\', \'set\');">' +
-        '<i class="fa fa-times"></i> <small>dismiss</small>' +
+        '<span class="fa-stack">' +
+            '<i class="fa fa-circle-thin fa-stack-2x"></i> ' +
+            '<i class="fa fa-times fa-stack-1x"></i> ' +
+        '</span> dismiss' +
       '</button>';
+  blurb = blurb + '<p>';
   blurb = blurb + '<a href="' + a.url + '" class="alert-link">';
-  blurb = blurb + a.message + '</a></div>';
+  if (a.icon != undefined) {
+    blurb = blurb +
+        '<span class="fa-stack fa-lg">' +
+            '<i class="fa fa-circle-thin fa-stack-2x"></i> ' +
+            '<i class="fa fa-' + a.icon + ' fa-stack-1x"></i> ' +
+        '</span>';
+  }
+  blurb = blurb + a.message + '</a></p></div>';
   $('.intro-header').prepend(blurb);
 });
