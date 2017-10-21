69967e54a214e33025efec5ed7710eb7d4f8a8f2
Ralph Bean <rbean@redhat.com>
2017-10-21 06:32:56 -0400

    Add the PM art calendar.

--- js/calendar.js
+++ js/calendar.js
@@ -10,6 +10,9 @@ $(document).ready(function() {
             googleCalendarId: 'wintonroad.org_ft37u5b6np28du780ijku12jb0@group.calendar.google.com',
             className: 'morning-art-project',
         }, {
+            googleCalendarId: 'wintonroad.org_ec2ibpfnn2lsqecp2u1onr4jr8@group.calendar.google.com',
+            className: 'afternoon-art-project',
+        }, {
             googleCalendarId: 'wintonroad.org_ik8kre6udmcq2k57bg1p67mo9s@group.calendar.google.com',
             className: 'school-year',
         }],
