cbb59d17ebc7d2a38b683ed5765108a99b0a8ab2
Chris Barajas <barajas@rogueamoeba.com>
2024-01-08 10:31:37 -0500

    Site updates
    - cropped Ally's photo
    - changed teacher images from circle to rounded design
    - updated enrollment steps
    - added enrollment date info
    - updated tuition
    - added program notes
    - hid enrollment PDF
    - adjusted form layout

--- js/announcements.js
+++ js/announcements.js
@@ -12,21 +12,30 @@ var announcements = [
       url: 'forms/wrns-application.pdf',
       icon: 'paperclip',
       expires: new Date(2022, 08, 31)
-  }  
+  },
+  {
+    idx: 'open-enrollment-2024',
+    message: 'Enrollment opens soon for the 2024–2025 school year!',
+    url: '#enroll',
+    icon: 'file',
+    expires: new Date("February 8, 2024 01:00:00")
+  }    
 ];
 $.each(announcements, function(i, a) {
   if (Cookies.get(a.idx) != undefined) return;
   if (a.expires < Date.now()) return;
-  var blurb = '<div class="announcement alert alert-dismissible alert-danger">' +
+  var blurb = '<div class="announcement info alert-dismissible alert-info">' +
       '<button type="button" class="close btn btn-sm mybutton_standard" data-dismiss="alert" ' +
         'onclick="javascript:Cookies.set(\'' + a.idx + '\', \'set\', {expires: 365});">' +
         '<span class="fa-stack">' +
             '<i class="fa fa-circle-thin fa-stack-2x"></i> ' +
             '<i class="fa fa-times fa-stack-1x"></i> ' +
-        '</span> dismiss' +
+        '</span>' +
       '</button>';
   blurb = blurb + '<p>';
-  blurb = blurb + '<a href="' + a.url + '" class="alert-link">';
+  if (a.url != undefined) {
+    blurb = blurb + '<a href="' + a.url + '" class="alert-link">';
+  }
   if (a.icon != undefined) {
     blurb = blurb +
         '<span class="fa-stack fa-lg">' +
@@ -34,6 +43,10 @@ $.each(announcements, function(i, a) {
             '<i class="fa fa-' + a.icon + ' fa-stack-1x"></i> ' +
         '</span>';
   }
-  blurb = blurb + a.message + '</a></p></div>';
+  blurb = blurb + a.message;
+  if (a.url != undefined) {
+    blurb = blurb + '</a>';
+  }
+  blurb = blurb + '</p></div>';
   $('.intro-header').prepend(blurb);
 });
