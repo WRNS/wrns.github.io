9b2036be4ecc128d6d3a47d24932cc49b75898f9
Mike Curtis <Mallekith@gmail.com>
2026-02-03 10:36:39 -0500

    changing admissions page dates.

--- src/app/admissions/page.js
+++ src/app/admissions/page.js
@@ -22,7 +22,7 @@ export default function Enrollment() {
                 svg.setAttribute("style", `fill: #fff`);
               }}
             />
-            <span className="btn__text-content">2025-2026 Admissions Information</span>
+            <span className="btn__text-content">2026-2027 Admissions Information</span>
           </Link>
           <div className="number-heading">
             <div className="number-heading__box">1</div>
